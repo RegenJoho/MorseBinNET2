aef20a3f99bf9e856e71959881f0430e267acc8a
RegenJoho <63968870+RegenJoho@users.noreply.github.com>
2021-09-03 17:39:47 +0200

    initial commit, working completely fine

--- MorseBinNET2E.cpp
+++ MorseBinNET2E.cpp
@@ -1,27 +1,48 @@
 #include "Arduino.h"
-#include "MorseBinNET2.h"
-MorseBinNET2::MorseBinNET2(String address, int pinTransmit){
+#include "MorseBinNET2E.h"
+MorseBinNET2E::MorseBinNET2E(String address, int pinTransmit){
 	_pinTransmit = pinTransmit;
 	_address = address;
 	pinMode(_pinTransmit,INPUT);
 	timeMiddle = ((time0+time1)/2);
 	timeForStart = ((time0+time1)-(time1/2));
 }
-void MorseBinNET2::send(String address, String data1, String data2){
-		sendByte(address, _pinTransmit);
-		sendByte(_address, _pinTransmit);
-		sendByte("00000000",_pinTransmit);
-		sendByte(data1, _pinTransmit);
-		sendByte(data2, _pinTransmit);
+MorseBinNET2E::MorseBinNET2E(String address, int pinTransmit, int speed){
+	_pinTransmit = pinTransmit;
+	_address = address;
+	pinMode(_pinTransmit,INPUT);
+	time1 = speed;
+	time0 = time1*3;
+	timeMiddle = ((time0+time1)/2);
+	timeForStart = ((time0+time1)-(time1/2));
 }
-void MorseBinNET2::send(String address,String busID, String data1, String data2){
-		sendByte(address, _pinTransmit);
-		sendByte(_address, _pinTransmit);
-		sendByte(busID,_pinTransmit);
-		sendByte(data1, _pinTransmit);
-		sendByte(data2, _pinTransmit);
+void MorseBinNET2E::send(String address, String data1, String data2, String data3, String data4, String data5, String data6, String data7, String data8) {
+	sendByte(address, _pinTransmit);
+	sendByte(_address, _pinTransmit);
+	sendByte("00000000",_pinTransmit);
+	sendByte(data1, _pinTransmit);
+	sendByte(data2, _pinTransmit);
+	sendByte(data3, _pinTransmit);
+	sendByte(data4, _pinTransmit);
+	sendByte(data5, _pinTransmit);
+	sendByte(data6, _pinTransmit);
+	sendByte(data7, _pinTransmit);
+	sendByte(data8, _pinTransmit);
 }
-void MorseBinNET2::receive(){
+void MorseBinNET2E::send(String address,String busID, String data1, String data2, String data3, String data4, String data5, String data6, String data7, String data8){
+	sendByte(address, _pinTransmit);
+	sendByte(_address, _pinTransmit);
+	sendByte(busID,_pinTransmit);
+	sendByte(data1, _pinTransmit);
+	sendByte(data2, _pinTransmit);
+	sendByte(data3, _pinTransmit);
+	sendByte(data4, _pinTransmit);
+	sendByte(data5, _pinTransmit);
+	sendByte(data6, _pinTransmit);
+	sendByte(data7, _pinTransmit);
+	sendByte(data8, _pinTransmit);
+			}
+void MorseBinNET2E::receive(){
 	String receiverAddress = receiveByte(_pinTransmit);
 	if((receiverAddress == "errTimeout")){
 		lastMessage[0] = "errTimeout";
@@ -72,37 +93,103 @@ void MorseBinNET2::receive(){
 			return;
 		}
 	}
+	String data3 = receiveByte(_pinTransmit);
+	if(data3 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data3 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
+	String data4 = receiveByte(_pinTransmit);
+	if(data4 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data4 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
+	String data5 = receiveByte(_pinTransmit);
+	if(data5 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data5 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
+	String data6 = receiveByte(_pinTransmit);
+	if(data6 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data6 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
+	String data7 = receiveByte(_pinTransmit);
+	if(data7 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data7 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
+	String data8 = receiveByte(_pinTransmit);
+	if(data8 == "errTimeout"){
+		lastMessage[0] = "errTimeout";
+		return;
+	} else {
+		if(data8 == "errGrabledMessage"){
+			lastMessage[0] = "errGrabledMessage";
+			return;
+		}
+	}
 	if(_address != receiverAddress){
 		lastMessage[0] = "errWrongAddress";
 		//Serial.println(_address + ":" + receiverAddress);
 		return;
 	}
 	if(senderBusID == "00000000"){
-		lastMessage[4] = "local";
+		lastMessage[2] = "local";
 	} else{
-		lastMessage[4] = senderBusID;
+		lastMessage[2] = senderBusID;
 	}
 	lastMessage[1] = senderAddress;
-	lastMessage[2] = data1;
-	lastMessage[3] = data2;
+	lastMessage[3] = data1;
+	lastMessage[4] = data2;
+	lastMessage[5] = data3;
+	lastMessage[6] = data4;
+	lastMessage[7] = data5;
+	lastMessage[8] = data6;
+	lastMessage[9] = data7;
+	lastMessage[10] = data8;
 	lastMessage[0] = "errSuccess";
 }
-String MorseBinNET2::placeRequest(String address, String data1, String data2) {
+String MorseBinNET2E::placeRequest(String address, String data1, String data2) {
 	return "errNotImplemented";
 }
-void MorseBinNET2::Send0(int sendpin) {
+void MorseBinNET2E::Send0(int sendpin) {
  	digitalWrite(sendpin,HIGH);
  	delayMicroseconds(time0);
  	digitalWrite(sendpin,LOW);
  	delayMicroseconds(time1);
   }
-void MorseBinNET2::Send1(int sendpin) {
+void MorseBinNET2E::Send1(int sendpin) {
  	digitalWrite(sendpin,HIGH);
  	delayMicroseconds(time1);
  	digitalWrite(sendpin,LOW);
  	delayMicroseconds(time1);
   }
-void MorseBinNET2::sendByte(String input,int sendpin){
+void MorseBinNET2E::sendByte(String input,int sendpin){
 	while(digitalRead(sendpin) == HIGH){
 		delayMicroseconds(time1+5);
 		//Serial.println("StuckInTraffic");
@@ -125,7 +212,7 @@ void MorseBinNET2::sendByte(String input,int sendpin){
     }
 	pinMode(sendpin,INPUT);
   }
-  String MorseBinNET2::receiveByte(int respin){
+  String MorseBinNET2E::receiveByte(int respin){
 	String buf;
   	int waitTimer = 0;
 	int timeouttimer = 0;
